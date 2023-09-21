dd9ccffda5a23bf889ef1f45095cf42d9a4a63cb
Tu Vu <tuvu852@gmail.com>
2023-09-20 19:20:26 -0700

    Add user model

--- user.go
+++ user.go
@@ -1,1 +1,19 @@
-package gochat
+package wealthdone
+
+import "time"
+
+// User represents a user account.
+type User struct {
+	ID int `json:"id"`
+
+	// User's basic information.
+	Name  string `json:"name"`
+	Email string `json:"email"`
+
+	// Password is the user's hashed password.
+	Password string `json:"password"`
+
+	// Timestamps for user creation & last update.
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
