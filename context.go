e8d5d26b2394387aec82721808228228a8142745
Tu Vu <tuvu852@gmail.com>
2023-09-20 19:19:59 -0700

    Add context handling

--- context.go
+++ context.go
@@ -1,6 +1,26 @@
-package gochat
+package wealthdone
+
+import "context"
 
 // contextKey represents an internal key for adding context fields.
-// This is considered best practice as it prevents other packages from
-// interfering with our context keys.
 type contextKey int
+
+// List of context keys, which are used to store request-scoped data.
+const (
+	// Stores the current logged-in user in the context.
+	userContextKey = contextKey(iota + 1)
+)
+
+// NewContextWithUser returns a new context with the given user.
+func NewContextWithUser(ctx context.Context, user *User) context.Context {
+	return context.WithValue(ctx, userContextKey, user)
+}
+
+// UserFromContext returns user stored in context, if any.
+func UserFromContext(ctx context.Context) *User {
+	u, ok := ctx.Value(userContextKey).(*User)
+	if !ok {
+		return nil
+	}
+	return u
+}
