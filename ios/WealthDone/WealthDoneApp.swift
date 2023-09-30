fd6b067d6b4240d76fe3a7ba56ab049b9b115656
Tu Vu <tuvu852@gmail.com>
2023-09-30 12:16:22 -0700

    Vim mode in Xcode woohoo!

---
+++ ios/WealthDone/WealthDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WealthDoneApp.swift
+//  WealthDone
+//
+//  Created by Tu Vu on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct WealthDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
